8165533697adfc7b9a179e6638d46dbfcad17cec
Himenesu <jhervinjimenez03@gmail.com>
2021-03-07 18:17:05 +0800

    Updated code

--- database/connect.php
+++ database/connect.php
@@ -5,8 +5,8 @@ define('DBUSERNAME', 'root');     //database username root
 
 $connect = mysqli_connect(DBSERVER, DBUSERNAME, "") or die ("Cannot connect to the server.");   //connection string
 
-$data = mysqli_query($connect, "CREATE DATABASE IF NOT EXISTS `registrationdata` ");
-$data = mysqli_query($connect, "
+$mydatabase = mysqli_query($connect, "CREATE DATABASE IF NOT EXISTS `registrationdata` ");
+$mydatabase = mysqli_query($connect, "
                     CREATE TABLE IF NOT EXISTS `registrationdata` . `datatable` (
                         `id` int(11) NOT NULL AUTO_INCREMENT,
                         `firstname` varchar(20) NOT NULL,
@@ -20,5 +20,9 @@ $data = mysqli_query($connect, "
 
 if($connect === false){
     die("Cannot connect to the database." . mysqli_error());
+}
+else{
+    mysqli_select_db($connect, "registrationdata"); //connection open
+    // echo "Conection Success"
 }
 ?>
