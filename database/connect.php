1276a2af567ee6a7939ab55a36cf29c7441ed57b
Himenesu <jhervinjimenez03@gmail.com>
2021-04-03 19:54:24 +0800

    added new files and codes, also added the terms and conditions

--- database/connect.php
+++ database/connect.php
@@ -5,19 +5,32 @@ define('DBUSERNAME', 'root');     //database username root
 
 $connect = mysqli_connect(DBSERVER, DBUSERNAME, "") or die ("Cannot connect to the server.");   //connection string
 
-$mydatabase = mysqli_query($connect, "CREATE DATABASE IF NOT EXISTS `registrationdata` ");
+$mydatabase = mysqli_query($connect, "CREATE DATABASE IF NOT EXISTS `registration_data` ");
 $mydatabase = mysqli_query($connect, "
-                    CREATE TABLE IF NOT EXISTS `registrationdata` . `usersdata` (
+                    CREATE TABLE IF NOT EXISTS `registration_data` . `users_personal_data` (
                         `id` int(11) NOT NULL AUTO_INCREMENT,
                         `username` varchar(10) NOT NULL,
+                        `pass` varchar(15) NOT NULL,
+                        `cpass` varchar(15) NOT NULL,
                         `firstname` varchar(20) NOT NULL,
                         `midname` varchar(20) NOT NULL,
                         `lastname` varchar(20) NOT NULL,
+                        `birthdate` varchar(20) NOT NULL,
+                        `birthplace` varchar(40) NOT NULL,
+                        `sex` varchar(10) NOT NULL,
+                        `civil` varchar(20) NOT NULL,
                         `email` varchar(30) NOT NULL,
                         `contact` int(11) NOT NULL,
-                        `address` varchar(25) NOT NULL,
-                        `pass` varchar(15) NOT NULL,
-                        `cpass` varchar(15) NOT NULL,
+                        `citizen` varchar(20) NOT NULL,
+                        `acquisition` varchar(20) NOT NULL,
+                        `province` varchar(40) NOT NULL,
+                        `city` varchar(40) NOT NULL,
+                        `brgy` varchar(30) NOT NULL,
+                        `address` varchar(40) NOT NULL,
+                        `postal` int(10) NOT NULL,
+                        `city_years_months_number` varchar(15) NOT NULL,
+                        `country_years_number` int(5) NOT NULL,
+                        
                         PRIMARY KEY(`id`)
                     ) ENGINE = InnoDB;");
 
@@ -25,7 +38,7 @@ if($connect === false){
     die("Cannot connect to the database." . mysqli_error());
 }
 else{
-    mysqli_select_db($connect, "registrationdata"); //connection open
+    mysqli_select_db($connect, "registration_data"); //connection open
     // echo "Conection Success"
 }
 ?>
--- secondpage.php
+++ secondpage.php
@@ -10,7 +10,7 @@
                 crossorigin="anonymous"
             ></script>
 
-                <link rel="stylesheet" href="statics/style2.css" />
+                <link rel="stylesheet" href="css/style2.css" />
                     <title> iRegister | Registration Form</title>
     </head>   
 <body>
@@ -25,7 +25,7 @@
             <h2>Personal Information</h2>
             <span class="help-block">(Personal na impormasyon)</span>
         </div> 
-
+            <p color=>Note: Make sure that all of the information are correct. Once you submit the form, you will not be able to edit it. You may contact us on our customer service email: iRegister.au@gmail.com, if you have any problems or questions. </a></p><br>
             <div class="form">
                <div class="inputfield">
                   <label>First Name:</label>
@@ -42,105 +42,10 @@
                <div class="inputfield">
                   <label>Birthdate:</label>
                   <input type="text" name="birthd" class="input" placeholder="Araw ng iyong kapanganakan" required>
-               </div>  
+               </div>   
                 <div class="inputfield">
-                  <label>Birth Place:</label>
-                  <div class="custom_select">
-                    <select name="bplace" required>
-                            <option value="">- Select -</option>
-                            <option value="99">OUTSIDE THE PHILIPPINES</option>
-                            <option value="39">NCR - 1st DISTRICT (Manila)</option>
-                            <option value="74">NCR - 2ND DISTRICT (Mandaluyong, Marikina, Pasig, QC, San Juan)</option>
-                            <option value="75">NCR - 3RD DISTRICT (Caloocan, Malabon, Navotas, Valenzuela)</option>
-                            <option value="76">NCR - 4TH DISTRICT (Las Piñas, Makati, Muntinlupa, Parañaque, Pasay, Pateros, Taguig)</option>
-                            <option value="01">ABRA</option>
-                            <option value="02">AGUSAN DEL NORTE</option>
-                            <option value="03">AGUSAN DEL SUR</option>
-                            <option value="04">AKLAN</option>
-                            <option value="05">ALBAY</option>
-                            <option value="06">ANTIQUE</option>
-                            <option value="81">APAYAO</option>
-                            <option value="77">AURORA</option>
-                            <option value="07">BASILAN</option>
-                            <option value="08">BATAAN</option>
-                            <option value="09">BATANES</option>
-                            <option value="10">BATANGAS</option>
-                            <option value="11">BENGUET</option>
-                            <option value="78">BILIRAN</option>
-                            <option value="12">BOHOL</option>
-                            <option value="13">BUKIDNON</option>
-                            <option value="14">BULACAN</option>
-                            <option value="15">CAGAYAN</option>
-                            <option value="16">CAMARINES NORTE</option>
-                            <option value="17">CAMARINES SUR</option>
-                            <option value="18">CAMIGUIN</option>
-                            <option value="19">CAPIZ</option>
-                            <option value="20">CATANDUANES</option>
-                            <option value="21">CAVITE</option>
-                            <option value="22">CEBU</option>
-                            <option value="82">COMPOSTELA VALLEY</option>
-                            <option value="47">COTABATO (NORTH COT.)</option>
-                            <option value="23">DAVAO (DAVAO DEL NORTE)</option>
-                            <option value="24">DAVAO DEL SUR</option>
-                            <option value="86">DAVAO OCCIDENTAL</option>
-                            <option value="25">DAVAO ORIENTAL</option>
-                            <option value="85">DINAGAT ISLANDS</option>
-                            <option value="26">EASTERN SAMAR</option>
-                            <option value="79">GUIMARAS</option>
-                            <option value="27">IFUGAO</option>
-                            <option value="28">ILOCOS NORTE</option>
-                            <option value="29">ILOCOS SUR</option>
-                            <option value="30">ILOILO</option>
-                            <option value="31">ISABELA</option>
-                            <option value="32">KALINGA</option>
-                            <option value="33">LA UNION</option>
-                            <option value="34">LAGUNA</option>
-                            <option value="35">LANAO DEL NORTE</option>
-                            <option value="36">LANAO DEL SUR</option>
-                            <option value="37">LEYTE</option>
-                            <option value="38">MAGUINDANAO</option>
-                            <option value="40">MARINDUQUE</option>
-                            <option value="41">MASBATE</option>
-                            <option value="42">MISAMIS OCCIDENTAL</option>
-                            <option value="43">MISAMIS ORIENTAL</option>
-                            <option value="44">MOUNTAIN PROVINCE</option>
-                            <option value="45">NEGROS OCCIDENTAL</option>
-                            <option value="46">NEGROS ORIENTAL</option>
-                            <option value="48">NORTHERN SAMAR</option>
-                            <option value="49">NUEVA ECIJA</option>
-                            <option value="50">NUEVA VIZCAYA</option>
-                            <option value="51">OCCIDENTAL MINDORO</option>
-                            <option value="52">ORIENTAL MINDORO</option>
-                            <option value="53">PALAWAN</option>
-                            <option value="54">PAMPANGA</option>
-                            <option value="55">PANGASINAN</option>
-                            <option value="56">QUEZON</option>
-                            <option value="57">QUIRINO</option>
-                            <option value="58">RIZAL</option>
-                            <option value="59">ROMBLON</option>
-                            <option value="60">SAMAR (WESTERN SAMAR)</option>
-                            <option value="80">SARANGANI</option>
-                            <option value="61">SIQUIJOR</option>
-                            <option value="62">SORSOGON</option>
-                            <option value="63">SOUTH COTABATO</option>
-                            <option value="64">SOUTHERN LEYTE</option>
-                            <option value="97">SPECIAL PROVINCE</option>
-                            <option value="65">SULTAN KUDARAT</option>
-                            <option value="66">SULU</option>
-                            <option value="67">SURIGAO DEL NORTE</option>
-                            <option value="68">SURIGAO DEL SUR</option>
-                            <option value="69">TARLAC</option>
-                            <option value="70">TAWI-TAWI</option>
-                            <option value="71">ZAMBALES</option>
-                            <option value="72">ZAMBOANGA DEL NORTE</option>
-                            <option value="73">ZAMBOANGA DEL SUR</option>
-                            <option value="83">ZAMBOANGA SIBUGAY</option>
-                        </select>
-                    </div>
-                </div> 
-                <div class="inputfield">
-                  <label>City/Municipality:</label>
-                  <input type="text" name="municipality" class="input">
+                  <label>Birthplace:</label>
+                  <input type="text" name="bplace" placeholder="Lugar kung saan ka ipinanganak" class="input">
                 </div>  
                 <div class="inputfield">
                   <label>Sex:</label>
@@ -339,7 +244,7 @@
                         <input type="checkbox" >
                         <span class="checkmark"></span>
                     </label>
-                    <p>Agreed to terms and conditions</p>
+                     <p>I agree to <a href="termsandconditions.php"> terms and conditions</a> </p>
                 </div> 
                 <div class="inputfield">
                     <input type="submit" value="Register" class="btn">
